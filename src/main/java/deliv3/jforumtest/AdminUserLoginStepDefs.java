c91e15d751a5ad1c20b6016853d0afe0e589a63f
Adnan <adnankhan@gmail.com>
2014-03-14 20:52:39 -0400

    fixed comments for step def skele

--- src/main/java/deliv3/jforumtest/AdminUserLoginStepDefs.java
+++ src/main/java/deliv3/jforumtest/AdminUserLoginStepDefs.java
@@ -1,5 +1,9 @@
 package deliv3.jforumtest;
 
 public class AdminUserLoginStepDefs extends SeleniumSteps{
+//	Given that I am a forum administrator
+//	When I try to log in
+//	and my login is accepted
+//	then I am able to access the admin control panel
 
 }
--- src/main/java/deliv3/jforumtest/PostAccessRestStepDefs.java
+++ src/main/java/deliv3/jforumtest/PostAccessRestStepDefs.java
@@ -1,5 +1,8 @@
 package deliv3.jforumtest;
 
 public class PostAccessRestStepDefs {
-
+//	Given that I am an admin
+//	and I make a post restricted to admins only
+//	when a non-administrator views the forum
+//	then the user is unable to see the restricted thread
 }
