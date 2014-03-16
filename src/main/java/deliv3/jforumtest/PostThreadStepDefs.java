cc0f8bec518a9dfbf6411202173d94801ce7e7a7
Adnan <adnankhan@gmail.com>
2014-03-16 09:04:26 -0400

    adding feature files

--- src/main/java/deliv3/jforumtest/PostThreadStepDefs.java
+++ src/main/java/deliv3/jforumtest/PostThreadStepDefs.java
@@ -1,5 +1,10 @@
 package deliv3.jforumtest;
 
 public class PostThreadStepDefs extends SeleniumSteps {
-
+//	Given that I am a user
+//	and I am trying to post a thread
+//	and I have permission to post a thread
+//	When I fill in the thread body
+//	and I post the thread
+//	Then the thread appears for other users to view
 }
